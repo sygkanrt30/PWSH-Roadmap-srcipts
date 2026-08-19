d2368cce662fcdc16c4a14db6fc948db7d4654fe
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2026-08-19 17:48:01 +0300

    Polishing

--- create_diary.ps1
+++ create_diary.ps1
@@ -6,12 +6,12 @@ $fileName = "diary.txt"
 
 Set-Location $desktopPath
 
+
 New-Item -Path $fileName -ItemType File
 
 Add-Content $fileName -Value "[2026-08-14 10:00] Начал изучать PowerShell`n[2026-08-14 11:00] Научился писать в файл`n[2026-08-14 12:00] Уже всё понимаю!" -Encoding utf8
 
+
 Write-host (Get-Content $fileName -Encoding utf8)
 
 Write-Host "Количество строк: $((Get-Content $fileName -Encoding utf8 | Measure-Object -Line).Lines)"
-
-Remove-Item $fileName
